47fcddb9ff3118d92baaff7c5bb642883ee47ccf
paul <rohanpaul2@gmail.com>
2022-05-19 15:58:56 +0530

    first commit

---
+++ config.py
@@ -0,0 +1,13 @@
+BATCH_SIZE = 16
+EPOCH = 10
+n_splits = 5
+fold_selected = 2  # 1,...,5
+
+
+#### ONLY CHANGE DIRECTORIES HERE AND NO NEED TO CHANGE IT ANYWHERE ELSE IN THIS NOTEBOOK #####
+# Kaggle Root Dir
+# TRAIN_ROOT_DIR = "../input/uw-madison-gi-tract-image-segmentation/"
+# TEST_ROOT_DIR = "../input/uw-madison-gi-tract-image-segmentation/test"
+
+TRAIN_ROOT_DIR = "../../input/uw-madison-gi-tract-image-segmentation/"
+TEST_ROOT_DIR = "../../input/uw-madison-gi-tract-image-segmentation/test/"
